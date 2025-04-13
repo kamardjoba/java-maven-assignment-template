83d54a0506144f0c95c7e84c5c83c2db00539138
kamardjoba <126898629+kamardjoba@users.noreply.github.com>
2025-04-13 19:15:54 +0200

    initial

--- src/test/java/example/CalculatorTest.java
+++ src/test/java/example/CalculatorTest.java
@@ -3,7 +3,7 @@ package example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalcuflatorTest {
+public class CalculatorTest {
 
     @Test
     public void testAdd() {
